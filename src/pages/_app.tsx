c438e6ea0096aac9b21d03417caec3afa99308c1
ilya <pasterzoom@gmail.com>
2023-08-25 10:28:44 +0300

    initial configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { MantineProvider } from '@mantine/core'
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import '../styles/globals.css'
+
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactElement
 }
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const App = ({ Component, pageProps }: AppPropsWithLayout): ReactElement => {
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return getLayout(
+    <>
+      <Head>
+        <link href="/favicon_32x32.png" rel="icon" />
+        <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
+}
+
+export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { ReactElement } from 'react'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import { createGetInitialProps } from '@mantine/next'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+const getInitialProps = createGetInitialProps()
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps
+
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
